6f29ef86e4b1ef90c758d415fda549dbaf5667a6
Sourav Majumder <souravmajumderk99@gmail.com>
2023-05-15 01:25:19 +0600

    backend done

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+
+async function main() {
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
+  await nftMarketplace.deployed();
+  console.log("NFTMarketplace deployed to:", nftMarketplace.address);
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../client/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = hre.artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
